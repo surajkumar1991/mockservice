d63958c716235eb33eeebc449a2762a27660a6ad
rahul sahu <rahul.sahu@vuclip.com>
2018-08-03 13:04:57 +0530

    Signed-off-by: rahul sahu <rahul.sahu@vuclip.com>

---
+++ src/main/java/com/vuclip/ubs/controller/AdNetworkService.java
@@ -0,0 +1,20 @@
+package com.vuclip.ubs.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdNetworkService {
+
+	@RequestMapping(value = "/conversion/{volumtid}/pixel.jpg", method = { RequestMethod.POST, RequestMethod.GET })
+	public ResponseEntity<String> conversionPost(@PathVariable String volumtid) {
+		System.out.println("ANS: GOT  REQUEST" + volumtid);
+
+		return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+	}
+
+}
