704d3e2697ca499606b6b4c9aba87f70d90c6484
Mayank <mayank.bharshiv@afourtech.com>
2018-08-20 15:19:14 +0530

    Signed-off-by: Mayank <mayank.bharshiv@afourtech.com>

--- src/main/java/com/vuclip/ubs/controller/SubscriptionServiceController.java
+++ src/main/java/com/vuclip/ubs/controller/SubscriptionServiceController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,6 @@ import com.vuclip.ubs.subscription_service.FreeTrialEligibilityRequestVO;
 import com.vuclip.ubs.subscription_service.FreeTrialEligibilityResponseVO;
 import com.vuclip.ubs.subscription_service.Response;
 import com.vuclip.ubs.subscription_service.SubscriptionStatusReponse;
-import com.vuclip.ubs.subscription_service.UserSubscriptionMapper;
 
 @RestController
 public class SubscriptionServiceController {
@@ -34,42 +32,43 @@ public class SubscriptionServiceController {
 			@RequestParam(required = false) String userid, @RequestParam(required = false) String msisdn,
 			@RequestParam(required = false) Long subscriptionId) {
 		logger.info("GETUSERSTATUS REQUEST PARAM : " + userid + msisdn + subscriptionId + productId);
-		String whereClause = "";
-		boolean u = false, m = false, s = false;
+		/*
+		 * String whereClause = ""; boolean u = false, m = false, s = false; if (userid
+		 * != null) { whereClause += " user_id='" + userid + "' and"; u = true; } if
+		 * (msisdn != null) { whereClause += " msisdn='" + msisdn + "'  and"; m = true;
+		 * } if (subscriptionId != null) { whereClause += " subscription_id='" +
+		 * subscriptionId + "'  and"; s = true; }
+		 * 
+		 * if (whereClause.contains("and")) whereClause = whereClause.substring(0,
+		 * whereClause.length() - 3); SubscriptionStatusReponse strresponse = null; if
+		 * (u || m || s) { String query =
+		 * "SELECT * FROM ubs_mock.user_subscription where " + whereClause; try {
+		 * strresponse = jdbcTemplate.queryForObject(query, new
+		 * UserSubscriptionMapper()); } catch (EmptyResultDataAccessException e) {
+		 * logger.info("No REcord found"); } }
+		 * 
+		 * if (strresponse == null) { return new SubscriptionStatusReponse(false,
+		 * "Error", "200", null);
+		 * 
+		 * } strresponse.setResponseCode("200"); strresponse.setSuccessful(true);
+		 * strresponse.setMessage("success"); logger.info("GET USER STATUS RESPONSE : "
+		 * + strresponse.toString()); return strresponse;
+		 */
+
 		if (userid != null) {
-			whereClause += " user_id='" + userid + "' and";
-			u = true;
+			String query = "SELECT * FROM user_subscription where user_id='" + userid + "'";
+			return getUserSubscriptionRecord(query);
 		}
 		if (msisdn != null) {
-			whereClause += " msisdn='" + msisdn + "'  and";
-			m = true;
+			String query = "SELECT * FROM user_subscription where msisdn='" + msisdn + "'";
+			return getUserSubscriptionRecord(query);
 		}
 		if (subscriptionId != null) {
-			whereClause += " subscription_id='" + subscriptionId + "'  and";
-			s = true;
+			String query = "SELECT * FROM user_subscription where subscription_id='" + subscriptionId + "'";
+			return getUserSubscriptionRecord(query);
 		}
+		return new SubscriptionStatusReponse(false, "Data is not valid", "FA004", null);
 
-		if (whereClause.contains("and"))
-			whereClause = whereClause.substring(0, whereClause.length() - 3);
-		SubscriptionStatusReponse strresponse = null;
-		if (u || m || s) {
-			String query = "SELECT * FROM ubs_mock.user_subscription where " + whereClause;
-			try {
-				strresponse = jdbcTemplate.queryForObject(query, new UserSubscriptionMapper());
-			} catch (EmptyResultDataAccessException e) {
-				logger.info("No REcord found");
-			}
-		}
-
-		if (strresponse == null) {
-			return new SubscriptionStatusReponse(false, "Error", "200", null);
-
-		}
-		strresponse.setResponseCode("200");
-		strresponse.setSuccessful(true);
-		strresponse.setMessage("success");
-		logger.info("GET USER STATUS RESPONSE : " + strresponse.toString());
-		return strresponse;
 	}
 
 	@RequestMapping(value = "/check/freeTrialEligibility", method = RequestMethod.GET, produces = "application/json")
@@ -108,7 +107,7 @@ public class SubscriptionServiceController {
 			logger.info("QUERY FOR FETCHING DATA " + query);
 			List<Map<String, Object>> respon = jdbcTemplate.queryForList(query);
 			if (respon.size() >= 1) {
-				Object jsonval = respon.get(0).get("");
+				Object jsonval = respon.get(0).get("json");
 				logger.info(jsonval);
 				FreeTrialEligibilityResponseVO response = ObjectMapperUtils.readValueFromString((String) jsonval,
 						FreeTrialEligibilityResponseVO.class);
@@ -124,5 +123,25 @@ public class SubscriptionServiceController {
 		return FreeTrialEligibilityResponseVO.builder().freeTrialEligibility(true)
 				.response(new Response(true, "SUCCESS", "200")).build();
 
+	}
+
+	private SubscriptionStatusReponse getUserSubscriptionRecord(String query) {
+		try {
+			logger.info("QUERY FOR FETCHING DATA " + query);
+			List<Map<String, Object>> respon = jdbcTemplate.queryForList(query);
+			if (respon.size() >= 1) {
+				Object jsonval = respon.get(0).get("json");
+				System.out.println(jsonval);
+				SubscriptionStatusReponse response = ObjectMapperUtils.readValueFromString((String) jsonval,
+						SubscriptionStatusReponse.class);
+				return response;
+			}
+		} catch (Exception e) {
+			logger.info("Excpetion:" + e.getMessage());
+			return new SubscriptionStatusReponse(false, "Data is not valid", "FA004", null);
+
+		}
+		logger.info("User Status New User");
+		return new SubscriptionStatusReponse(true, "success", "200", null);
 	}
 }
