321ae4f80646bd7c58e56e6178d96f74837b609a
Mayank <mayank.bharshiv@afourtech.com>
2018-08-23 13:46:33 +0530

    Signed-off-by: Mayank <mayank.bharshiv@afourtech.com>

--- src/main/java/com/vuclip/ubs/subscription_service/DeactivateRequestVO.java
+++ src/main/java/com/vuclip/ubs/subscription_service/DeactivateRequestVO.java
@@ -1,7 +1,11 @@
 package com.vuclip.ubs.subscription_service;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author Akash Kadam
@@ -9,6 +13,10 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeactivateRequestVO {
 
 	String userId;
