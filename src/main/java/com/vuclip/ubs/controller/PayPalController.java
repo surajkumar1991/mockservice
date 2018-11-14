af1aab20865ba40a1a4cb9255858c9be22d75821
Rahul Sahu <rahulsahu@PUN-IT000501.local>
2018-11-14 18:16:04 +0530

    Signed-off-by: Rahul Sahu <rahulsahu@PUN-IT000501.local>

--- src/main/java/com/vuclip/ubs/controller/PayPalController.java
+++ src/main/java/com/vuclip/ubs/controller/PayPalController.java
@@ -15,65 +15,67 @@ import java.util.Map;
 @RestController
 public class PayPalController {
 
-    private Logger logger = LogManager.getLogger(PayPalController.class);
+	private static String authResponse = "{ \"scope\": \"client_credentials\","
+			+ "  \"nonce\": \"client_credentials\"," + "  \"access_token\": \"dummyaccesstoken\","
+			+ "  \"token_type\": \"Bearer\"," + "  \"app_id\": \"APP-VIU\"," + "  \"expires_in\": 323333333398" + "}";
+	private Logger logger = LogManager.getLogger(PayPalController.class);
 
-    @RequestMapping(value = "paypal/v1/oauth2/token", method = {RequestMethod.GET, RequestMethod.POST}, produces = {"application/json"})
-    public @ResponseBody
-    PayPalAuthResponse paypalAuth(HttpServletRequest request) throws IOException {
+	@RequestMapping(value = "paypal/v1/oauth2/token", method = { RequestMethod.GET, RequestMethod.POST }, produces = {
+			"application/json" })
+	public @ResponseBody PayPalAuthResponse paypalAuth(HttpServletRequest request) throws IOException {
 
-        logger.info("PayPal oauth2 Request");
-        Enumeration<String> headerNames = request.getHeaderNames();
+		logger.info("PayPal oauth2 Request");
+		Enumeration<String> headerNames = request.getHeaderNames();
 
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            logger.info("header name " + headerName);
-            Enumeration<String> headers = request.getHeaders(headerName);
-            while (headers.hasMoreElements()) {
-                String headerValue = headers.nextElement();
-                logger.info(headerValue);
-            }
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			logger.info("header name " + headerName);
+			Enumeration<String> headers = request.getHeaders(headerName);
+			while (headers.hasMoreElements()) {
+				String headerValue = headers.nextElement();
+				logger.info(headerValue);
+			}
 
-        }
-        
+		}
 
-        Map<String, String[]> parameters = request.getParameterMap();
-        for (String key : parameters.keySet()) {
-            logger.info("Key " + key);
-            for (String value : parameters.get(key)) {
-                logger.info("Value " + value);
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        BufferedReader reader = request.getReader();
-        try {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append('\n');
-            }
-        } finally {
-            reader.close();
-        }
-        logger.info(sb.toString());
-       
-        PayPalAuthResponse response = ObjectMapperUtils.readValue("classpath:PaypalAuthResponse.json", PayPalAuthResponse.class);
+		Map<String, String[]> parameters = request.getParameterMap();
+		for (String key : parameters.keySet()) {
+			logger.info("Key " + key);
+			for (String value : parameters.get(key)) {
+				logger.info("Value " + value);
+			}
+		}
 
-        return response;
-    }
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line).append('\n');
+			}
+		} finally {
+			reader.close();
+		}
+		logger.info(sb.toString());
 
-    @RequestMapping(value = "paypal/v1/payments/billing-agreements/{paymentToken}/agreement-execute", method = {RequestMethod.GET}, produces = {"application/json"})
-    public @ResponseBody
-    String paypalAgreementExecute(@PathVariable String paymentToken) {
-        logger.info("Payment Token : " + paymentToken);
-        return null;
-    }
+		PayPalAuthResponse response = ObjectMapperUtils.readValueFromString(authResponse,
+				PayPalAuthResponse.class);
 
-    @RequestMapping(value = "paypal/v1/payments/billing-agreements/<agreementId>/cancel", method = {RequestMethod.GET}, produces = {"application/json"})
-    public @ResponseBody
-    String paypalAgreementCancle(@PathVariable String agreementId) {
-        logger.info("agreementId  : " + agreementId);
-        return null;
-    }
+		return response;
+	}
 
+	@RequestMapping(value = "paypal/v1/payments/billing-agreements/{paymentToken}/agreement-execute", method = {
+			RequestMethod.GET }, produces = { "application/json" })
+	public @ResponseBody String paypalAgreementExecute(@PathVariable String paymentToken) {
+		logger.info("Payment Token : " + paymentToken);
+		return null;
+	}
+
+	@RequestMapping(value = "paypal/v1/payments/billing-agreements/<agreementId>/cancel", method = {
+			RequestMethod.GET }, produces = { "application/json" })
+	public @ResponseBody String paypalAgreementCancle(@PathVariable String agreementId) {
+		logger.info("agreementId  : " + agreementId);
+		return null;
+	}
 
 }
