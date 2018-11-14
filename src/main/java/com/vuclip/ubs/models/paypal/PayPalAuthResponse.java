c58a4291432bee76c13ff34efd62e6e89a7e0f2e
Rahul Sahu <rahulsahu@PUN-IT000501.local>
2018-11-14 16:37:17 +0530

    Signed-off-by: Rahul Sahu <rahulsahu@PUN-IT000501.local>

--- src/main/java/com/vuclip/ubs/models/paypal/PayPalAuthResponse.java
+++ src/main/java/com/vuclip/ubs/models/paypal/PayPalAuthResponse.java
@@ -5,80 +5,80 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "scope", "nonce", "access_token", "token_type", "app_id", "expires_in" })
+@JsonPropertyOrder({"scope", "nonce", "access_token", "token_type", "app_id", "expires_in"})
 public class PayPalAuthResponse {
 
-	@JsonProperty("scope")
-	private String scope;
-	@JsonProperty("nonce")
-	private String nonce;
-	@JsonProperty("access_token")
-	private String accessToken;
-	@JsonProperty("token_type")
-	private String tokenType;
-	@JsonProperty("app_id")
-	private String appId;
-	@JsonProperty("expires_in")
-	private Integer expiresIn;
+    @JsonProperty("scope")
+    private String scope;
+    @JsonProperty("nonce")
+    private String nonce;
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("app_id")
+    private String appId;
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
 
-	@JsonProperty("scope")
-	public String getScope() {
-		return scope;
-	}
+    @JsonProperty("scope")
+    public String getScope() {
+        return scope;
+    }
 
-	@JsonProperty("scope")
-	public void setScope(String scope) {
-		this.scope = scope;
-	}
+    @JsonProperty("scope")
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
 
-	@JsonProperty("nonce")
-	public String getNonce() {
-		return nonce;
-	}
+    @JsonProperty("nonce")
+    public String getNonce() {
+        return nonce;
+    }
 
-	@JsonProperty("nonce")
-	public void setNonce(String nonce) {
-		this.nonce = nonce;
-	}
+    @JsonProperty("nonce")
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
 
-	@JsonProperty("access_token")
-	public String getAccessToken() {
-		return accessToken;
-	}
+    @JsonProperty("access_token")
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-	@JsonProperty("access_token")
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
+    @JsonProperty("access_token")
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 
-	@JsonProperty("token_type")
-	public String getTokenType() {
-		return tokenType;
-	}
+    @JsonProperty("token_type")
+    public String getTokenType() {
+        return tokenType;
+    }
 
-	@JsonProperty("token_type")
-	public void setTokenType(String tokenType) {
-		this.tokenType = tokenType;
-	}
+    @JsonProperty("token_type")
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 
-	@JsonProperty("app_id")
-	public String getAppId() {
-		return appId;
-	}
+    @JsonProperty("app_id")
+    public String getAppId() {
+        return appId;
+    }
 
-	@JsonProperty("app_id")
-	public void setAppId(String appId) {
-		this.appId = appId;
-	}
+    @JsonProperty("app_id")
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
 
-	@JsonProperty("expires_in")
-	public Integer getExpiresIn() {
-		return expiresIn;
-	}
+    @JsonProperty("expires_in")
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
 
-	@JsonProperty("expires_in")
-	public void setExpiresIn(Integer expiresIn) {
-		this.expiresIn = expiresIn;
-	}
+    @JsonProperty("expires_in")
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
 
 }
