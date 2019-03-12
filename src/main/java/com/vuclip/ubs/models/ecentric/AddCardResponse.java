89084f91ceb6c81ed5154197502a527f184899ef
Paresh Danej <pareshdanej@PUN-IT000735.local>
2019-03-12 15:55:24 +0530

    reformating

--- src/main/java/com/vuclip/ubs/models/ecentric/AddCardResponse.java
+++ src/main/java/com/vuclip/ubs/models/ecentric/AddCardResponse.java
@@ -1,14 +1,13 @@
 
 package com.vuclip.ubs.models.ecentric;
 
-import lombok.Builder;
-import lombok.ToString;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+
+import lombok.ToString;
 
 
 /**
--- src/main/java/com/vuclip/ubs/models/ecentric/ResponseDetail.java
+++ src/main/java/com/vuclip/ubs/models/ecentric/ResponseDetail.java
@@ -1,13 +1,12 @@
 
 package com.vuclip.ubs.models.ecentric;
 
-import lombok.Builder;
-import lombok.ToString;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+
+import lombok.ToString;
 
 
 /**
