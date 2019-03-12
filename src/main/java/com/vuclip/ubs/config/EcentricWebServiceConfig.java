123f89693abc5b7982b73ca499f4389b0e591974
Paresh Danej <pareshdanej@PUN-IT000735.local>
2019-03-12 10:33:35 +0530

    updated ecentric web service config

--- src/main/java/com/vuclip/ubs/config/EcentricWebServiceConfig.java
+++ src/main/java/com/vuclip/ubs/config/EcentricWebServiceConfig.java
@@ -1,6 +1,7 @@
 package com.vuclip.ubs.config;
 
-import com.vuclip.ubs.interceptor.MessageHeaderInterceptor;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
@@ -15,8 +16,7 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
-import java.io.IOException;
-import java.util.List;
+import com.vuclip.ubs.interceptor.MessageHeaderInterceptor;
 
 
 @EnableWs
@@ -48,7 +48,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @return
      */
     @Bean
-    public XsdSchema addCardSchema() {
+    public XsdSchema ecentricSchema() {
         return new SimpleXsdSchema(rs);
     }
 
@@ -58,7 +58,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @throws IOException
      */
     @Bean(name = "AddCardRequest")
-    public DefaultWsdl11Definition defaultAddCardRequestDefinition(XsdSchema addCardSchema) throws IOException {
+    public DefaultWsdl11Definition defaultAddCardRequestDefinition(XsdSchema addCardSchema) {
         DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
         definition.setPortTypeName("AddCardRequest");
         definition.setTargetNamespace("http://www.ecentricswitch.co.za/paymentgateway/v1");
@@ -74,7 +74,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @throws IOException
      */
     @Bean(name = "Secure3DLookupRequest")
-    public DefaultWsdl11Definition defaultSecure3dLookUpRequestDef(XsdSchema secure3DLookupSchema) throws IOException {
+    public DefaultWsdl11Definition defaultSecure3dLookUpRequestDef(XsdSchema secure3DLookupSchema) {
         DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
         definition.setPortTypeName("Secure3DLookupRequest");
         definition.setTargetNamespace("http://www.ecentricswitch.co.za/paymentgateway/v1");
@@ -91,7 +91,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @throws IOException
      */
     @Bean(name = "AuthorizeRequest")
-    public DefaultWsdl11Definition defaulAuthorizeRequestDef(XsdSchema authorizeSchema) throws IOException {
+    public DefaultWsdl11Definition defaulAuthorizeRequestDef(XsdSchema authorizeSchema)  {
         DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
         definition.setPortTypeName("AuthorizeRequest");
         definition.setTargetNamespace("http://www.ecentricswitch.co.za/paymentgateway/v1");
@@ -107,7 +107,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @throws IOException
      */
     @Bean(name = "VoidRequest")
-    public DefaultWsdl11Definition defaulVoidRequestDef(XsdSchema voidSchema) throws IOException {
+    public DefaultWsdl11Definition defaulVoidRequestDef(XsdSchema voidSchema)  {
         DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
         definition.setPortTypeName("VoidRequest");
         definition.setTargetNamespace("http://www.ecentricswitch.co.za/paymentgateway/v1");
@@ -123,7 +123,7 @@ public class EcentricWebServiceConfig extends WsConfigurerAdapter {
      * @throws IOException
      */
     @Bean(name = "PaymentRequest")
-    public DefaultWsdl11Definition defaulPaymentRequestDef(XsdSchema paymentSchema) throws IOException {
+    public DefaultWsdl11Definition defaulPaymentRequestDef(XsdSchema paymentSchema) {
         DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
         definition.setPortTypeName("PaymentRequest");
         definition.setTargetNamespace("http://www.ecentricswitch.co.za/paymentgateway/v1");
